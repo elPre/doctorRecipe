1fe0163ce9f506c6fae748ce08d4e2793e225020
hectorleyvavillanueva <hleyva@gmail.com>
2021-10-06 16:33:24 -0500

    Adding the first page MVP populated

---
+++ app/src/main/java/com/recippie/doctor/app/activity/BaseActivity.kt
@@ -0,0 +1,61 @@
+package com.recippie.doctor.app.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var isSaveInstanceStateCalled: Boolean = false
+    protected open val useDefaultOrientationHandling = true
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isSaveInstanceStateCalled = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        isSaveInstanceStateCalled = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isSaveInstanceStateCalled = false
+    }
+
+    protected fun showFragment(@IdRes layout: Int, fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(layout, fragment, tag)
+            .commit()
+    }
+}
+
+abstract class BaseBindingActivity<VB : ViewBinding> : BaseActivity() {
+
+    private var _binding: ViewBinding? = null
+    abstract val bindingInflater: (LayoutInflater) -> VB
+
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater.invoke(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.clearViews()
+        _binding = null
+    }
+
+    protected open fun VB.clearViews() = Unit
+}
--- app/src/main/java/com/recippie/doctor/app/fragment/RecipeFragment.kt
+++ app/src/main/java/com/recippie/doctor/app/fragment/RecipeFragment.kt
@@ -5,29 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.recippie.doctor.app.adapter.RecipeAdapter
 import com.recippie.doctor.app.databinding.RecipeFragmentBinding
 import com.recippie.doctor.app.event.RecipeActionEvent
 import com.recippie.doctor.app.viewmodel.RecipeViewModel
 
-class RecipeFragment: BaseBindingFragment<RecipeFragmentBinding>() {
+class RecipeFragment : BaseBindingFragment<RecipeFragmentBinding>() {
 
-    //private val adapter = RecipeAdapter(::onAction)
-    //private val viewModel: RecipeViewModel by viewModels()
+    private val adapter = RecipeAdapter(::onAction)
+    private val viewModel: RecipeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //viewModel.loadRecipes()
+        val isNewFragment = savedInstanceState == null
+        viewModel.loadRecipesItems(isNewFragment)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            rvRecipe.layoutManager = LinearLayoutManager(context).apply {
+                orientation = LinearLayoutManager.VERTICAL
+            }
+            rvRecipe.adapter = adapter
+        }
+        viewModel.recipeList.observe(viewLifecycleOwner) {
+            adapter.setData(it)
+        }
     }
 
     override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?) =
         RecipeFragmentBinding.inflate(inflater, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        viewModel.recipeList.observe(viewLifecycleOwner) {
-//            adapter.setData(it)
-//        }
-    }
 
     private fun onAction(action: RecipeActionEvent) {
 
@@ -37,5 +46,4 @@ class RecipeFragment: BaseBindingFragment<RecipeFragmentBinding>() {
         const val TAG = "RecipeFragment"
         fun newInstance() = RecipeFragment()
     }
-
 }
