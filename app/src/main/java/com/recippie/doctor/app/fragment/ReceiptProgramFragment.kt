98066566187f43bd17d332b07a9c1ee004f1b56b
hectorleyvavillanueva <hleyva@gmail.com>
2021-10-21 12:48:54 -0500

    Adding the android widgets timepicker and datepicker

--- app/src/main/java/com/recippie/doctor/app/fragment/ReceiptProgramFragment.kt
+++ app/src/main/java/com/recippie/doctor/app/fragment/ReceiptProgramFragment.kt
@@ -5,17 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.recippie.doctor.app.adapter.ProgramAdapter
 import com.recippie.doctor.app.databinding.ReceiptProgramFragmentBinding
 import com.recippie.doctor.app.event.ReceiptActionEvent
 import com.recippie.doctor.app.viewmodel.ProgramViewModel
 
-class ReceiptProgramFragment: BaseBindingFragment<ReceiptProgramFragmentBinding>() {
+class ReceiptProgramFragment : BaseBindingFragment<ReceiptProgramFragmentBinding>() {
 
     private val adapter = ProgramAdapter(::onAction)
     private val viewModel: ProgramViewModel by viewModels()
 
-    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?)=
+    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?) =
         ReceiptProgramFragmentBinding.inflate(inflater, container, false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,12 +36,43 @@ class ReceiptProgramFragment: BaseBindingFragment<ReceiptProgramFragmentBinding>
     }
 
     private fun onAction(action: ReceiptActionEvent) {
-        when(action){
+        when (action) {
             ReceiptActionEvent.SaveProgram -> Unit
-            ReceiptActionEvent.OpenCalendar -> Unit
-            ReceiptActionEvent.OpenClock -> Unit
+            ReceiptActionEvent.OpenCalendar -> showDatePicker()
+            ReceiptActionEvent.OpenClock -> showTimePicker()
             ReceiptActionEvent.ProgramSchedule -> viewModel.loadSchedule()
+            else -> Unit
         }
+    }
+
+    private fun showTimePicker() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Start Alarm Time")
+            .build().apply {
+                addOnPositiveButtonClickListener {
+                    onTimeSelected(this)
+                }
+            }
+        timePicker.show(childFragmentManager, TAG)
+    }
+
+    private fun onTimeSelected(timePicker: MaterialTimePicker) {
+        viewModel.setTime(timePicker)
+    }
+
+
+    private fun showDatePicker() {
+        MaterialDatePicker.Builder.datePicker().setSelection(System.currentTimeMillis()).build().apply {
+            addOnPositiveButtonClickListener { dateInMillis -> onDateSelected(dateInMillis) }
+        }.show(childFragmentManager, TAG)
+    }
+
+
+    private fun onDateSelected(dateTimeStampInMillis: Long) {
+        viewModel.setDate(dateTimeStampInMillis)
     }
 
     companion object {
